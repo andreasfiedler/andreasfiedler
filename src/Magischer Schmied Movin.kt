0c31ffa18439c5f9066f308946c7a97fca7a3ec0
andreasfiedler <fiedler.a51@gmail.com>
2023-11-02 13:41:33 +0100

    The Black World

--- src/Magischer Schmied Movin.kt
+++ src/Magischer Schmied Movin.kt
@@ -5,21 +5,21 @@ class Movin(name: String, gesundheit: Int) : Held(name, gesundheit) {
     }
 
 
-    fun spezialAttacke() {
-        val spezialAttack: MutableMap<String, Int> = mutableMapOf(
+
+        val spezialAttacke: MutableMap<String, Int> = mutableMapOf(
         "Kettenschwert" to 10,
         "Blaster Kanone" to 15,
         "Raketen Armbrust" to 30
     )
-        //println("$name verwendet seine Spezialfunktion ${spezialAttacke}!")
-    }
+
+
 
     override fun attack(): Int {
-
-        //val spezial = spezialAttacke().entries.random()
+        // Wählt zufällig einen der Spezialangriffe aus
+        val spezial = spezialAttacke.entries.random()
         //val schaden = spezialAttacke.entries.random()
-        println("$name verwendet seine Spezialfunktion ${spezialAttacke()} ")
-        return super.attack()
+        println("$name verwendet seine Spezialfunktion ${spezial.key} ")
+        return spezial.value
     }
 }
 
