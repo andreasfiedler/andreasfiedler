60cb1aece87c041eefe2dbdd76d6443d0140f1ab
andreasfiedler <fiedler.a51@gmail.com>
2023-11-07 13:28:04 +0100

    The Black World

--- src/Dogahn.kt
+++ src/Dogahn.kt
@@ -7,6 +7,8 @@ class Dogahn(name: String, gesundheit: Int) : Held(name, gesundheit) {
         "Feuriger Atem" to 12,
         "Tiefflug" to 30
     )
+
+    // Helden Attacken werden zufällig aufgerufen
     override fun attack(): Int {
         val spezial = spezialAttacke.entries.random()
         println("$name verwendet seine Spezialfunktion ${spezial.key} ")
