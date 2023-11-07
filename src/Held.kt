e6f2a207c486d53bff410ecbe0ab76a37ef2ed3c
andreasfiedler <fiedler.a51@gmail.com>
2023-11-07 13:25:10 +0100

    The Black World

--- src/Held.kt
+++ src/Held.kt
@@ -3,13 +3,13 @@
 open class Held(val name: String, var gesundheit: Int) {
     open val beutel = Beutel("Heilzauber", 5, "Zauber", 30)
     open fun attack(): Int {
-        // Zufällige Angriffsstärke zwischen 1 und 10
+        // Zufällige Angriffsstärke zwischen 1 und 12
         val damage = (1..12).random()
 
         println("$name greift an und verursacht $damage Schaden!")
         return damage
     }
-
+    // Verteidigen und verbleibende Gesundheit. Wird in der while Schleife ausgegeben
     fun verteidigen(damage: Int) {
         gesundheit -= damage
         println("$name hat jetzt $gesundheit Gesundheitspunkte.")
